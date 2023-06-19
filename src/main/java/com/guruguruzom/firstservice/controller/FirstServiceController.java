6dfb9bac86ebf78c223c3a032c081dc696acc882
guruguruzom <guruguruzom@gmail.com>
2023-06-19 22:49:40 +0900

    load balancing 동작 확인 로직 추가

--- src/main/java/com/guruguruzom/firstservice/controller/FirstServiceController.java
+++ src/main/java/com/guruguruzom/firstservice/controller/FirstServiceController.java
@@ -1,15 +1,24 @@
 package com.guruguruzom.firstservice.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 @Slf4j
 public class FirstServiceController {
+
+    //application.yml 파일에 선언된 변수 가져오기
+    private final Environment env;
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First service";
@@ -22,7 +31,11 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "First Service Check";
+    public String check(HttpServletRequest request){
+        //mvc를 통한 방법
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("First Service Check on Port %s"
+                , env.getProperty("local.server.port"));
     }
 }
